2340b12591c5529a6c323dc82c1745dc6090d827
Taishi Kobari <70504137+TaishiKobari@users.noreply.github.com>
2024-12-04 09:43:56 +0000

    BaseMachinaで管理されている外部ライブラリの型定義を追加

--- gcs-boilerplate/types.d.ts
+++ gcs-boilerplate/types.d.ts
@@ -1,2 +1,6 @@
 // `@basemachina/view` の型定義は公開されていないので、モジュールの宣言だけ行う
 declare module "@basemachina/view";
+// 外部ライブラリのモジュールの宣言を行う
+declare module "dayjs";
+declare module "@chakra-ui/react";
+declare module "react-chartjs-2";
